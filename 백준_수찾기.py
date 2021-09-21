9d12881811b7fbbd1f62c0f907c78919f1ad051f
IT-HONGREAT <hongreat95@gmail.com>
2021-09-21 15:45:34 +0900

    set으로 풀이

--- 백준_수찾기.py
+++ 백준_수찾기.py
@@ -2,20 +2,32 @@
 # 4 1 5 2 3
 # 5
 # 1 3 7 9 5
+#
+# n = int(input())
+# num = {}
+# ns = list(map(int,input().split(" ")))
+# for i in ns:
+#     num[i] = 1
+#
+# m = int(input())
+#
+# ms = list(map(int,input().split(" ")))
+#
+# for j in ms:
+#     if j in num.keys():
+#         print(num[j])
+#     else:
+#         print(0)
+
+#set풀이
 
 n = int(input())
-num = {}
-ns = list(map(int,input().split(" ")))
-for i in ns:
-    num[i] = 1
-
+array = set(map(int,input().split()))
 m = int(input())
+x  = list(map(int,input().split()))
 
-ms = list(map(int,input().split(" ")))
-
-for j in ms:
-    if j in num.keys():
-        print(num[j])
+for i in x:
+    if i not in array:
+        print("0")
     else:
-        print(0)
-
+        print("1")
