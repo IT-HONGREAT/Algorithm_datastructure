dbd34f4b21795ad41c779d6f288552c31dd387c8
hong-in-yeong <hongreat95@gmail.com>
2021-09-30 19:41:51 +0900

    함수를 사용하면 최적화 될 줄 알았으나, 메모리와 속도측면에서 모두 동일, 로직이 같기 때문에 이런 것 같다.

--- 백준_트로피진열.py
+++ 백준_트로피진열.py
@@ -35,3 +35,29 @@ for r in prizes_R:
 
 print(len(answer_L))
 print(len(answer_R))
+
+
+
+
+#함수로 최적화_시도
+n = int(input())
+
+def find(list):
+
+    now = list[0]
+    result = 1
+    for i in range(1,len(list)):
+        if now < list[i]:
+            result += 1
+            now = list[i]
+    return result
+
+prizes_L= []
+for i in range(n):
+    prize = int(input())
+    prizes_L.append(prize)
+
+prizes_R = prizes_L[::-1]
+
+print(find(prizes_L))
+print(find(prizes_R))
