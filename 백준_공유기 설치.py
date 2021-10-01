cb90ecc60b68c019c12964acfff17de9b5cccca1
hong-in-yeong <hongreat95@gmail.com>
2021-10-01 17:34:36 +0900

    이진탐색 알고리즘을 이용해서 연산 자체를 최적화

---
+++ 백준_공유기 설치.py
@@ -0,0 +1,41 @@
+# 5 3
+# 1
+# 2
+# 8
+# 4
+# 9
+
+N, C = map(int,input().split())
+
+house = []
+
+for i in range(N):
+    house.append(int(input()))
+house = sorted(house)
+
+min_gap = house[1] - house[0]  #초기 8
+max_gap = house[-1] - house[0] #초기 1
+result = 0
+
+while min_gap <= max_gap:
+    mid = (min_gap + max_gap) // 2
+    value = house[0]
+    count = 1
+
+    for i in range(1, len(house)):
+        if house[i] >= value + mid:
+            value = house[i]
+            count += 1
+
+    if count >= C:  #설치 가능
+        min_gap = mid + 1
+        result = mid
+    else:  #설치 불가
+        max_gap = mid -1
+
+
+# print(min_gap)
+# print(max_gap)
+# print(house)
+print(result)
+
