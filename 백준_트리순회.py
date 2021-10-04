f073a1d21d43f01f70980415f8e901b0ac7d0a40
IT-HONGREAT <hongreat95@gmail.com>
2021-10-04 23:10:44 +0900

    클래스와 dict를 이용해서 전위순회, 중위순회, 후위순회 구현한 아주 좋은 문제이다.

--- 백준_트리순회.py
+++ 백준_트리순회.py
@@ -11,4 +11,49 @@
 # 중위 순회한 결과 : DBAECFG // (왼쪽 자식) (루트) (오른쪽 자식)
 # 후위 순회한 결과 : DBEGFCA // (왼쪽 자식) (오른쪽 자식) (루트)
 
+# 클래스로 트리구조 틀 만들기
+
+class Node:
+    def __init__(self, data, left_node, right_node):
+        self.data = data
+        self.left_node = left_node
+        self.right_node = right_node
+
+#전위순회 함수
+
+def pre_order(node):  # 재귀로 구현한다.
+    print(node.data,end='')
+    if node.left_node != '.':
+        pre_order(tree[node.left_node])
+    if node.right_node != '.':
+        pre_order(tree[node.right_node])
+
+def in_order(node):
+    if node.left_node != '.':
+        in_order(tree[node.left_node])
+    print(node.data,end='')
+    if node.right_node != '.':
+        in_order(tree[node.right_node])
+
+def post_order(node):
+    if node.left_node != '.':
+        post_order(tree[node.left_node])
+    if node.right_node != '.':
+        post_order(tree[node.right_node])
+    print(node.data,end='')
+
+
 n = int(input())
+tree = {}
+
+for _ in range(n):
+    data, left_node, right_node = input().split()
+    tree[data] = Node(data, left_node, right_node) # 트리의 각 데이터의 노드에 대한 정보를 담아둔다.(자신,왼쪽자식,오른쪽자식)
+
+
+
+pre_order(tree["A"])
+print()
+in_order(tree["A"])
+print()
+post_order(tree["A"])
