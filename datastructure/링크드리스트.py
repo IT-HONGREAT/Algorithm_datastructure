5c48631c12bc2885a2e10b18d0e4127a0b3f4904
hong-in-yeong <hongreat95@gmail.com>
2021-10-14 16:43:41 +0900

    링크드 리스트에 insert 메소드를 작성했는데, 인덱스 값으로 노드를 찾는 메소드의 출력값이 2로 고정되는 에러가 생긴다!! 현 상태 저장 및 원인을 찾아봐야겠다.

--- datastructure/링크드리스트.py
+++ datastructure/링크드리스트.py
@@ -5,31 +5,31 @@ class Node:
         self.data = data
         self.next = None
 
-# 기본 노드 생성
-head_node = Node(1)
-node_1 = Node(3)
-node_2 = Node(5)
-node_3 = Node(7)
-node_4 = Node(9)
-tail_node = Node(11)
+# # 기본 노드 생성
+# head_node = Node(1)
+# node_1 = Node(3)
+# node_2 = Node(5)
+# node_3 = Node(7)
+# node_4 = Node(9)
+# tail_node = Node(11)
+#
+# #next노드연결
+#
+# head_node.next = node_1
+# node_1.next = node_2
+# node_2.next = node_3
+# node_3.next = node_4
+# node_4.next = tail_node
+#
+# #기본 링크드 리스트 출력
+# iterator = head_node
 
-#next노드연결
-
-head_node.next = node_1
-node_1.next = node_2
-node_2.next = node_3
-node_3.next = node_4
-node_4.next = tail_node
-
-#기본 링크드 리스트 출력
-iterator = head_node
 
 
-
-while iterator is not None:
-    print("데이터출력 : ",iterator.data)
-    print("다음데이터 : ",iterator.next)
-    iterator = iterator.next
+# while iterator is not None:
+#     print("데이터출력 : ",iterator.data)
+#     print("다음데이터 : ",iterator.next)
+#     iterator = iterator.next
 
 print("====="*20)
 
@@ -40,25 +40,38 @@ class LinkedList:
         self.head = None
         self.tail = None
 
+    def insert_after(self, previous_node, data):
+        """ 링크드 리스트에서 노드 사이에 새로운 노드 추가"""
+        new_node = Node(data)
+
+        #맨 뒤에 삽입 될 때
+        if previous_node is self.tail:
+            self.tail.next = new_node
+            self.tail = new_node
+        #노드 사이에 삽입 될 때
+        else:
+            new_node.next = previous_node.next
+            previous_node.next = new_node
+
     def find_node_at(self,index):
         #파라미터 인덱스의 위치에 있는 노드 출력
-        iterator3 = self.head
+        iterator = self.head
 
         for _ in range(index):
-            iterator3 = iterator3.next
+            iterator = iterator.next
 
-            return iterator3
+            return iterator
 
     def find_node_with_data(self, data):
         """링크드 리스트에서 탐색 연산 메소드. 단, 해당 노드가 없으면 None을 리턴한다"""
 
-        iterator3 = self.head
+        iterator = self.head
 
-        while iterator3 is not None:
+        while iterator is not None:
 
-            if iterator3.data == data:
-                return iterator3
-            iterator3 = iterator3.next
+            if iterator.data == data:
+                return iterator
+            iterator = iterator.next
 
         return None
 
@@ -79,46 +92,66 @@ class LinkedList:
     #링크드리스트를 문자열로 정리
     def __str__(self):
         res_str = "|"
-        iterator3 = self.head
+        iterator = self.head
 
-        while iterator3 is not None:
-            res_str += f'{iterator3.data} |'
-            iterator3 = iterator3.next
+        while iterator is not None:
+            res_str += f'{iterator.data} |'
+            iterator = iterator.next
 
         return res_str
 
 
 
-
-new_list = LinkedList()
-
-new_list.append(2)
-new_list.append(4)
-new_list.append(6)
-new_list.append(8)
-new_list.append(10)
-print("문자열로 출력된 링크드 리스트 : " , new_list)
-
-print("원하는 노드의 인덱스 값을 넣으면 노드값이 출력 : ", new_list.find_node_at(3).data)
-
-
-linked_list = LinkedList()
-
-# 여러 데이터를 링크드 리스트 마지막에 추가
-linked_list.append(2)
-linked_list.append(3)
-linked_list.append(5)
-linked_list.append(7)
-linked_list.append(11)
-node_with_11 = linked_list.find_node_with_data(11)
-
-if not node_with_11 is None:
-    print(node_with_11.data)
-else:
-    print("11를 갖는 노드는 없습니다")
-# iterator2 = new_list.head
 #
-# while iterator2 is not None:
-#     print("링크드리스트 추가 후 출력 : " ,iterator2.data)
-#     print("다음데이터 : ",iterator2.next)
-#     iterator2 = iterator2.next
+# new_list = LinkedList()
+#
+# new_list.append(2)
+# new_list.append(4)
+# new_list.append(6)
+# new_list.append(8)
+# new_list.append(10)
+# print("문자열로 출력된 링크드 리스트 : " , new_list)
+#
+# print("원하는 노드의 인덱스 값을 넣으면 노드값이 출력 : ", new_list.find_node_at(3).data)
+# print("====="*20)
+#
+# linked_list = LinkedList()
+#
+# # 여러 데이터를 링크드 리스트 마지막에 추가
+# linked_list.append(3)
+# linked_list.append(6)
+# linked_list.append(9)
+# linked_list.append(12)
+# linked_list.append(15)
+#
+# node_with_11 = linked_list.find_node_with_data(11)
+#
+# if not node_with_11 is None:
+#     print(node_with_11.data)
+# else:
+#     print("11를 갖는 노드는 없습니다")
+
+print("====="*20)
+#insert 테스트위한 새로운 링크드 리스트 생성
+
+my_list = LinkedList()
+
+#노드 추가
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+my_list.append(7)
+
+
+print("my_list : ",my_list)
+
+node_2 = my_list.find_node_at(2)
+my_list.insert_after(node_2,6)
+
+print(my_list)
+
+node_4 = my_list.find_node_at(4)
+print("node_4 : ", node_4.data)
+
+my_list.insert_after(node_4,10)
+print(my_list)
